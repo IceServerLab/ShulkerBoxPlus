18bfe9ba7ee0968ee107d7d0b310c39d76f17256
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-07-24 22:18:10 +0900

    [Fix] 手に持っているアイテムがチェストなどにも入れられないバグを修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "jp.iceserver"
-version = "1.0.0"
+version = "1.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
--- src/main/kotlin/jp/iceserver/shulkerboxplus/listeners/InventoryClick.kt
+++ src/main/kotlin/jp/iceserver/shulkerboxplus/listeners/InventoryClick.kt
@@ -4,6 +4,8 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.ItemStack
 
 class InventoryClick : Listener
 {
@@ -13,7 +15,14 @@ class InventoryClick : Listener
     @EventHandler
     fun onInventoryClick(e: InventoryClickEvent)
     {
-        if (e.currentItem != (e.whoClicked as Player).inventory.itemInMainHand) return
+        if (e.inventory.type != InventoryType.SHULKER_BOX) return
+
+        val currentItem: ItemStack = e.currentItem ?: return
+        val itemInHand = (e.whoClicked as Player).inventory.itemInMainHand
+
+        if (currentItem != itemInHand) return
+        if (!currentItem.type.name.contains("SHULKER_BOX")) return
+        if (!itemInHand.type.name.contains("SHULKER_BOX")) return
 
         e.isCancelled = true
     }
