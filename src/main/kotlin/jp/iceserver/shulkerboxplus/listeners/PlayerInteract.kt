cdae02a0f97551e45959cc983d5204141f13ed9a
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-08-25 23:49:13 +0900

    [Fix] 置かれたシュルカーボックスをシュルカーボックスを持ったまま開けないように

--- src/main/kotlin/jp/iceserver/shulkerboxplus/listeners/PlayerInteract.kt
+++ src/main/kotlin/jp/iceserver/shulkerboxplus/listeners/PlayerInteract.kt
@@ -1,5 +1,8 @@
 package jp.iceserver.shulkerboxplus.listeners
 
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.block.Block
 import org.bukkit.block.ShulkerBox
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -16,10 +19,14 @@ class PlayerInteract : Listener
     @EventHandler
     fun onPlayerInteract(e: PlayerInteractEvent)
     {
-        if (e.action != Action.RIGHT_CLICK_AIR) return
-
         val item: ItemStack = e.item ?: return
         if (!item.type.name.contains("SHULKER_BOX")) return
+        if ((e.clickedBlock ?: e.player.location.block).type.name.contains("SHULKER_BOX"))
+        {
+            e.isCancelled = true
+            return
+        }
+        if (e.action != Action.RIGHT_CLICK_AIR) return
 
         e.player.openInventory(((item.itemMeta as BlockStateMeta).blockState as ShulkerBox).inventory)
     }
